cf0667686672e1ab02ccb7acf4cfca7ade61e786
PSock7 <papesock@yahoo.fr>
2023-05-27 16:04:25 +0200

    refont app

--- Masterapp/app/src/main/java/com/project/masterapp/LoginActivity.kt
+++ Masterapp/app/src/main/java/com/project/masterapp/LoginActivity.kt
@@ -1,11 +1,16 @@
 package com.project.masterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun Registerdirection(view: View) {
+        startActivity(Intent(this, RegistrationActivity::class.java))
     }
 }
--- Masterapp/app/src/main/java/com/project/masterapp/WelcomeActivity.kt
+++ Masterapp/app/src/main/java/com/project/masterapp/WelcomeActivity.kt
@@ -2,11 +2,21 @@ package com.project.masterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.content.Intent
+import android.view.View
+
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+    }
+    fun register(view: View) {
+        startActivity(Intent(this, RegistrationActivity::class.java))
+    }
+    fun login(view: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 
 }
