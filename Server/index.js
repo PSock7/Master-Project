c2f6b496535829d8fbb8246d3371621c527e75d8
xDev <papesock@yahoo.fr>
2023-06-04 11:15:20 +0200

    fix index bug

--- Server/index.js
+++ Server/index.js
@@ -26,19 +26,20 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
+const authRouter = require("./routes/authRoutes");
+app.use("/api/v1/auth", authRouter);
 
 app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
+  res.status(404)
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' })
+  } else {
+      res.type('txt').send('404 Not Found')
+  }
 })
-const authRoutter = require("./routes/authRoutes");
-app.use("/api/v1/auth", authRoutter);
+
 
 app.use(errorHandler)
 
