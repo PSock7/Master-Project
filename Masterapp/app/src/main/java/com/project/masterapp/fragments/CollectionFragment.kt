671384961bcf1703cc63d1daf40b142745cbc9aa
CREATOR-1-618 <62265798+CREATOR-1-618@users.noreply.github.com>
2023-06-23 16:04:24 +0200

    cool

--- Masterapp/app/src/main/java/com/project/masterapp/fragments/CollectionFragment.kt
+++ Masterapp/app/src/main/java/com/project/masterapp/fragments/CollectionFragment.kt
@@ -27,9 +27,9 @@ class CollectionFragment(
         val plantlist = arrayListOf<PlantModel>()
 
         //enregistrer dans notre liste pissenlit
-        plantlist.add(PlantModel("tulipe", "belle plante", "https://cdn.pixabay.com/photo/2018/04/29/18/24/tulip-3360748_1280.jpg", false))
+        plantlist.add(PlantModel("tulipe", "belle plante", "https://cdn.pixabay.com/photo/2018/04/29/18/24/tulip-3360748_1280.jpg", true))
         plantlist.add(PlantModel("rose", "valentin", "https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_1280.jpg", false))
-        plantlist.add(PlantModel("cactus", "ça pique", "https://cdn.pixabay.com/photo/2015/04/10/17/03/pots-716579_1280.jpg", false))
+        plantlist.add(PlantModel("cactus", "ça pique", "https://cdn.pixabay.com/photo/2015/04/10/17/03/pots-716579_1280.jpg", true))
         plantlist.add(PlantModel("tournesol", "soleil", "https://cdn.pixabay.com/photo/2016/07/23/00/12/sunflower-1536088_1280.jpg", false))
 
         val collectionRecyclerView = view?.findViewById<RecyclerView>(R.id.collection_recycler_list)
